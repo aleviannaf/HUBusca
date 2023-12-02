8e6f76d8a0ed9ae37cb6b8b209c7a395c0b91535
aleviannaf <alexandrevianna511@gmail.com>
2023-12-01 21:32:06 -0400

    feat: added confi to tastack and style global

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import GlobalStyles from './styles/GlobalStyles'
 import App from './App.tsx'
-import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+
+const query = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+     <QueryClientProvider client={query}>
+      <GlobalStyles/>
     <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
