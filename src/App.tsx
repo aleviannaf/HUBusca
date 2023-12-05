df60e918fde8791c223a28759d9832eca434568b
aleviannaf <alexandrevianna511@gmail.com>
2023-12-05 12:20:53 -0400

    feat: configured routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import './App.css'
+import { RouterProvider } from "react-router-dom"
+import router from "./routes"
 
 function App() {
   return (
-    <>
-    </>
+    <RouterProvider router={router} />
   )
 }
 
