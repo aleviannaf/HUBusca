6b4d7062fe09c9d51873a215aa4eb26a1a1d9932
aleviannaf <alexandrevianna511@gmail.com>
2023-12-05 12:21:11 -0400

    feat: provider to redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { store } from './store/index.ts'
 import GlobalStyles from './styles/GlobalStyles'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
@@ -9,9 +11,11 @@ const query = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+       <Provider store={store}>
      <QueryClientProvider client={query}>
       <GlobalStyles/>
     <App />
     </QueryClientProvider>
+    </Provider>
   </React.StrictMode>,
 )
